4e1ea7fd68cb2b4b2c9efcec0eb2302b808f50de
Brandon Yates <brandonyates@usf.edu>
2021-07-15 00:24:30 -0400

    Add project files.

---
+++ CPMS/Models/Author.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+//All the data fields to submit a new author
+namespace CPMS.Models
+{
+    public class Author
+    {
+        public Author()
+        {
+            Paper = new HashSet<Paper>();
+        }
+
+        [Key]
+        [Column("AuthorID")]
+        public int AuthorId { get; set; }
+
+        [StringLength(50)]
+        [Required]
+        [RegularExpression("[A-Za-z]+")]//letters only
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [StringLength(1)]
+        [Display(Name = "MI")]
+        [RegularExpression("[A-Za-z]+")]//letters only
+        public string MiddleInitial { get; set; }
+
+        [StringLength(50)]
+        [Required]
+        [Display(Name = "Last Name")]
+        [RegularExpression("[A-Za-z]+")]//letters only
+        public string LastName { get; set; }
+
+        [StringLength(50)]
+        [RegularExpression(@"^[a-zA-z0-9\s]*$")]//letters, numbers and space
+        public string Affiliation { get; set; }
+
+        [StringLength(50)]
+        [RegularExpression(@"^[a-zA-z0-9\s]*$")]//letters, numbers and space
+        public string Department { get; set; }
+
+        [StringLength(50)]
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9\s-.]*$")]//letters, space, numbers, and dash
+        public string Address { get; set; }
+
+        [StringLength(50)]
+        [Required]
+        [RegularExpression(@"^[a-zA-z\s]*$")]//letters and space
+        public string City { get; set; }
+
+        [StringLength(2)]
+        [Required]
+        [RegularExpression("[A-Za-z]+")]//letters only
+        public string State { get; set; }
+
+        [StringLength(10)]
+        [Display(Name = "Zip Code")]
+        [RegularExpression(@"^[0-9-]*$")]//numbers and dashes only
+        public string ZipCode { get; set; }
+
+        [StringLength(50)]
+        [Required]
+        [Display(Name = "Phone Number")]
+        [RegularExpression(@"^[0-9-()]*$")]//numbers ,dashes, parentheses
+        public string PhoneNumber { get; set; }
+
+        [StringLength(100)]
+        [Required]
+        [Display(Name = "Email")]
+        public string EmailAddress { get; set; }
+
+        [StringLength(5)]
+        [Required]
+        public string Password { get; set; }
+
+        [InverseProperty("Author")]
+        public virtual ICollection<Paper> Paper { get; set; }
+    }
+}
---
+++ CPMS/Startup.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CPMS.Data;
+using CPMS.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CPMS
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<CPMSDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("CPMS")));
+
+            // cookie policy and authentication
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.ConsentCookie.IsEssential = true;  //login cookie is marked essential
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(cookieOptions => {
+                cookieOptions.Cookie.Name = "UserLoginCookie";
+                cookieOptions.LoginPath = "/";
+            });
+
+
+            // require authorization for table use
+            services.AddRazorPages(options =>
+            {
+                options.Conventions.AuthorizeFolder("/Authors");
+                options.Conventions.AuthorizeFolder("/Papers");
+                options.Conventions.AuthorizeFolder("/Reviews");
+                options.Conventions.AuthorizeFolder("/Reviewers");
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
